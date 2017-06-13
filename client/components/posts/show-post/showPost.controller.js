079e69d2a97e946a51e32cff4c5678c8c3e557c4
katemontha <katemontha@gmail.com>
2017-06-13 11:25:15 -0400

    added more css

--- client/components/posts/show-post/showPost.controller.js
+++ client/components/posts/show-post/showPost.controller.js
@@ -9,19 +9,33 @@ function ShowPostController($state, $stateParams, PostsService) {
 
         PostsService.getSinglePostById(postIdToShow)
             .then(
-                function success(response) {
-                    vm.postEntry = response.data;
-                    console.log(response.data);
-                },
-                function failure(response) {
-                    console.log('Failed to retrieve information for Post with ID of ' + expenseIdToShow)
-                }
+            function success(response) {
+                vm.postEntry = response.data;
+                console.log(response.data);
+            },
+            function failure(response) {
+                console.log('Failed to retrieve information for Post with ID of ' + expenseIdToShow)
+            }
             )
     }
     initialize();
 
     vm.editPostEntry = function (postEntryId) {
         $state.go('edit_post/:postId', { postId: postEntryId });
+    }
+    
+    vm.deletePostEntry = function (postIndexToDelete, postIdToDeleteFromDatabase) {
+        PostsService.deleteIdFromDatabase(postIdToDeleteFromDatabase)
+            .then(
+            function success(response) {
+
+                vm.postEntries.splice(postIndexToDelete, 1);
+            },
+            function failure(response) {
+
+                console.log('Error deleting Post with ID of ' + postIdToDeleteFromDatabase);
+            }
+            )
     }
 }
 
